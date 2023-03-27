351f169f506543249f40469d35b424d8367d708f
Hridoy <hridoysheikh800@gmail.com>
2023-03-27 01:40:29 -0500

    new add

---
+++ variable.js
@@ -0,0 +1,8 @@
+var name = 'hridoy'
+var age = 22
+
+
+console.log(name +' knows a little bit javascript');
+console.log('his age is only' + age );
+console.log( name + ' is learning js from stack learner you tube channel');
+console.log('but his age is onlyyy ' + age);
